b7c744923e73413b3c1ba1c56359a00398b12a79
Johan Piet <johan.piet@gmail.com>
2018-01-17 13:31:54 +0100

    Updated the delete-app-definitions script to use bigboat URL instead of project name

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ Contains various tools for working with bigboat
 
 Usages:
 
-`docker run --rm ictu/bigboat-tools ./delete-app-definitions.sh $appName $project $apiKey $tag`
+`docker run --rm ictu/bigboat-tools ./delete-app-definitions.sh $appName $bigboatUrl $apiKey $tag`
 
 or
 
-Make sure you have bash, curl and jq installed and run: `./delete-app-definitions.sh $appName $project $apiKey $tag`
+Make sure you have bash, curl and jq installed and run: `./delete-app-definitions.sh $appName $bigboatUrl $apiKey $tag`
 
 Arguments:
 
 * _$appName_ The name of the application definition to delete versions for. This is the name specifief in the Bigboat Compose file
-* _$project_ The name of the project to delete application definitions for. Example: for `http://www.dashboard.isd.ictu/` the project name is 'isd'.
+* _$bigboatUrl_ The url of Bigboat to delete application definitions from. Example: `http://www.dashboard.example.org/`.
 * _$apiKey_ The API key used to communicate with Bigboat. This can be the deploy-key are an API Key found under the menu 'Configuration' in Bigboat.
 * _$tag_ Application definitions with this tag will be skipped. **_All_** other application definitions will be deleted.
--- delete-app-definitions.sh
+++ delete-app-definitions.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 appName=$1
-project=$2
+bigboatUrl=${2%/}
 apiKey=$3
 tag=$4
 
-appDefinitions=`curl -sS -XGET "http://www.dashboard.$project.ictu/api/v2/apps/" -H "api-key:$apiKey"`
+appDefinitions=`curl -sS -XGET "$bigboatUrl/api/v2/apps/" -H "api-key:$apiKey"`
 numberOfApps=`echo $appDefinitions | jq "[.[] | select(.name==\"$appName\")] | length"`
 numberOfApps=`expr $numberOfApps - 1`
 for i in `seq 0 ${numberOfApps}`; 
 do
   appVersion=`echo $appDefinitions | jq "[.[] | select(.name==\"${appName}\")][${i}]" | jq '.version' -r`
-  appDefinition=`curl -sS -XGET "http://www.dashboard.$project.ictu/api/v2/apps/$appName/$appVersion/files/bigboatCompose" -H "api-key:$apiKey"`
+  appDefinition=`curl -sS -XGET "$bigboatUrl/api/v2/apps/$appName/$appVersion/files/bigboatCompose" -H "api-key:$apiKey"`
   echo $appDefinition | sed 's/^.*tags:\s*//' | sed 's/\s*\w*:.*//' | grep -sw "$tag"
   found=$?
 
   if [ $found == 1 ]; 
   then
     echo delete $appName:$appVersion
-    curl -sS -XDELETE "http://www.dashboard.$project.ictu/api/v2/apps/$appName/$appVersion" -H "api-key:$apiKey"
+    curl -sS -XDELETE "$bigboatUrl/api/v2/apps/$appName/$appVersion" -H "api-key:$apiKey"
   else
     echo keep $appName:$appVersion
   fi
